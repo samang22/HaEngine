74e96cb02ed702ba68bf9f6e307f46125c4d4c1b
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-12-10 15:03:54 +0900

    FrontEngineMain

--- Engine/FrontendEngine.build.cs
+++ Engine/FrontendEngine.build.cs
@@ -84,3 +84,13 @@ public class FrontendEngineSolution : Solution
         conf.SetStartupProject<FrontendEngineProject>();
     }
 }
+
+public static class Main
+{
+    [Sharpmake.Main]
+    public static void SharpmakeMain(Sharpmake.Arguments arguments)
+    {
+        KitsRootPaths.SetUseKitsRootForDevEnv(DevEnv.vs2022, KitsRootEnum.KitsRoot10, Options.Vc.General.WindowsTargetPlatformVersion.Latest);
+        arguments.Generate<FrontendEngineSolution>();
+    }
+}
